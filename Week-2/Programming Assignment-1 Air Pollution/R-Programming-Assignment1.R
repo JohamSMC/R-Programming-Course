1e1c696de16e86c678679ecb5340a86bf980962e
Joham Medina <joham.smc@gmail.com>
2021-02-01 11:20:34 -0500

    Programming-Assignment-1 Air-Pollution - Part 2

--- Week-2/Programming Assignment-1 Air Pollution/R-Programming-Assignment1.R
+++ Week-2/Programming Assignment-1 Air Pollution/R-Programming-Assignment1.R
@@ -34,3 +34,64 @@ pollutantmean("specdata", "sulfate", 1:10)
 pollutantmean("specdata", "nitrate", 70:72)
 # > pollutantmean("specdata", "nitrate", 70:72)
 # [1] 1.706047
+
+
+
+# Part 2
+# Write a function that reads a directory full of files and reports the number 
+# of completely observed cases in each data file. The function should return a 
+# data frame where the first column is the name of the file and the second 
+# column is the number of complete cases. A prototype of this function follows
+
+complete <- function(directory, id = 1:332) {
+  
+  ## Set the path where the 'specdata' folder is located
+  workPath = "/home/sebastian/Documentos/CURSOS/Cursos-Coursera/R-Programming/Week-2/Programming Assignment-1 Air Pollution/specdata"
+  
+  ## obtaining the required files by storing them into mydata variable
+  mydata <- list.files(path = workPath)[id]
+  
+  ## create an empty vector
+  frows <- c()
+  counter <- 1
+  
+  for(i in mydata) {
+    
+    ## storing each .csv file without the NA values into fineobject variable
+    fineobject <- na.omit(read.csv(i))
+    
+    ## store the number of the fineobject rows in the empty f(ine)rows vector
+    frows[counter] <- nrow(fineobject)
+    counter <- counter + 1
+  }
+  ## creating and printing the f(ine)list
+  flist <- data.frame("id" = id, "nobs" = frows)
+  print(flist)
+}
+
+  # TESTS
+
+complete("specdata", 1)
+# > complete("specdata", 1)
+# id nobs
+# 1  1  117
+
+complete("specdata", c(2, 4, 8, 10, 12))
+# > complete("specdata", c(2, 4, 8, 10, 12))
+# id nobs
+# 1  2 1041
+# 2  4  474
+# 3  8  192
+# 4 10  148
+# 5 12   96
+
+complete("specdata", 30:25)
+# > complete("specdata", 30:25)
+# id nobs
+# 1 30  932
+# 2 29  711
+# 3 28  475
+# 4 27  338
+# 5 26  586
+# 6 25  463
+
